b7b559682e6053c75be4b48887ee54b032aee706
Nickolas Nunes Gonçalves <nickolas.dev@hotmail.com>
2025-05-26 12:04:30 -0300

    adição da funcionalidade do menu suspenso

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ document.querySelectorAll('.nav-menu a').forEach(link => {
         navMenu.classList.remove('active');
     });
 });
+
+// Variáveis para controlar o menu na rolagem/mouse
+const header = document.querySelector('header');
+let lastScrollY = window.scrollY;
+
+header.classList.add('visible');
+
+// Event listener para rolagem da página
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > lastScrollY && window.scrollY > 100) {
+        header.classList.remove('visible');
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+        header.classList.add('visible');
+    }
+    lastScrollY = window.scrollY;
+});
+
+// Event listener para movimento do mouse
+let mouseMoveTimeout;
+document.addEventListener('mousemove', (e) => {
+
+    if (e.clientY < 100) {
+        header.classList.remove('hidden');
+        header.classList.add('visible');
+        clearTimeout(mouseMoveTimeout);
+    } else if (header.classList.contains('visible') && window.scrollY > 100) {
+
+        clearTimeout(mouseMoveTimeout);
+        mouseMoveTimeout = setTimeout(() => {
+            header.classList.remove('visible');
+            header.classList.add('hidden');
+        }, 500);
+    }
+});
